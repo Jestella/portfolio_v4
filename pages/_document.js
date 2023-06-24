e4240dc45a303fd028da5c63304ffc6f33b99a30
jestella <jestellaa@gmail.com>
2023-06-24 11:29:02 -0400

    add google analytics tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,41 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
         />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap"
+          rel="stylesheet"
         />
+        {/*Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4SBZJ9BPXR"
+        ></script>
+        <script>
+          {`
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', 'G-4SBZJ9BPXR');
+          `}
+        </script>
       </Head>
       <body>
         <Main />
