a5d248065b1c150e0af427b2dac41583dae3eed2
jestella <jestellaa@gmail.com>
2023-06-24 11:34:27 -0400

    update google analytics tag id

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,7 @@ export default function Document() {
           {`
           window.dataLayer = window.dataLayer || []; function gtag()
           {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-4SBZJ9BPXR');
+          gtag('js', new Date()); gtag('config', 'G-8WQEWEDH2V');
           `}
         </script>
       </Head>
