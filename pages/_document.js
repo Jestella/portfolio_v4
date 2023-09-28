8230ba63b2528839955c4818050b988f32cb4ac3
jestella <jestellaa@gmail.com>
2023-09-28 17:23:06 -0400

    update google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -27,14 +27,12 @@ export default function Document() {
         {/*Google tag (gtag.js) */}
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-8WQEWEDH2V"
+          src="https://www.googletagmanager.com/gtag/js?id=G-5H1DNF7FDR"
         ></script>
         <script>
-          {`
           window.dataLayer = window.dataLayer || []; function gtag()
           {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-8WQEWEDH2V');
-          `}
+          gtag('js', new Date()); gtag('config', 'G-5H1DNF7FDR');
         </script>
       </Head>
       <body>
