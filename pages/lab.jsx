4d942368f383bd7c24358718f317842545b6456e
jestella <jestellaa@gmail.com>
2023-12-04 01:35:17 -0500

    update mobile styles

--- pages/lab.jsx
+++ pages/lab.jsx
@@ -4,11 +4,6 @@ import Navbar from "../components/ui/Navbar";
 import Button from "../components/ui/Button";
 
 function Lab() {
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
   return (
     <div className="lab">
       <Navbar />
@@ -18,9 +13,6 @@ function Lab() {
         <Link href="/">
           <Button>Home</Button>
         </Link>
-        {/* <div>
-          <button onClick={handleClick}>{likes} Likes</button>
-        </div> */}
       </div>
     </div>
   );
